e8adb2d58cbbb5a85a10326f1a0e5c405b9d686f
Zₐₗgₒ <45021556+jayden8923@users.noreply.github.com>
2019-01-27 20:12:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,12 @@ const client = new Discord.Client();
 
 client.once('ready', () => {
 	console.log('Im ready my leader');
-	client.music = require("discord.js-musicbot-addon");
-
-client.music.start(client, {
-youtubeKey: "AIzaSyB3yMFEIoUiCDy9hqUDhF6evukIOetV6Ms"
 });
 client.on('message', message => {
 	if (message.content === 'help') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send(`https://discord.gg/t4yqy26`);
-        
+
     };
 });
 client.login('INSERT BOT TOKEN');
